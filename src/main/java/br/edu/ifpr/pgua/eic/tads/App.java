29e7e4e66ea83e422c0e39c15d22c48a4072f0c6
mitugui <cristian.mitugui04@gmail.com>
2024-12-06 21:20:48 -0300

    feat: implementar sistema de cadastro de pessoas com listagem - Criado o modelo `Pessoa` e classe `Cadastro` para gerenciar uma lista de pessoas, com métodos para adicionar e recuperar registros. - Criado o controlador `CadastroController` para gerenciar as rotas relacionadas ao cadastro: - Rota GET `/cadastro` para renderizar o formulário de cadastro. - Rota POST `/cadastro` para adicionar uma pessoa e exibir a lista de pessoas cadastradas. - Atualizada a classe `App` para incluir as novas rotas e o controlador `CadastroController`. - Criado o formulário HTML `cadastro.html` para permitir o envio de dados via `POST`. - Criada a página `resposta.html` para exibir a lista de pessoas cadastradas.

--- src/main/java/br/edu/ifpr/pgua/eic/tads/App.java
+++ src/main/java/br/edu/ifpr/pgua/eic/tads/App.java
@@ -1,6 +1,8 @@
 package br.edu.ifpr.pgua.eic.tads;
 
+import br.edu.ifpr.pgua.eic.tads.controllers.CadastroController;
 import br.edu.ifpr.pgua.eic.tads.controllers.IndexController;
+import br.edu.ifpr.pgua.eic.tads.models.Cadastro;
 import br.edu.ifpr.pgua.eic.tads.utils.JavalinUtils;
 import io.javalin.Javalin;
 
@@ -11,10 +13,16 @@ import io.javalin.Javalin;
 public class App {
     public static void main( String[] args ){
         var app = JavalinUtils.makeApp(7070);
+
+        Cadastro cadastro = new Cadastro();
         
         IndexController indexController = new IndexController();
+        CadastroController cadastroController = new CadastroController(cadastro);
         
         app.get("/", indexController.get);
         app.get("/helloworld", indexController.ola);
+
+        app.get("/cadastro", cadastroController.get);
+        app.post("/cadastro", cadastroController.post);
     }
 }
---
+++ src/main/java/br/edu/ifpr/pgua/eic/tads/models/Pessoa.java
@@ -0,0 +1,19 @@
+package br.edu.ifpr.pgua.eic.tads.models;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
